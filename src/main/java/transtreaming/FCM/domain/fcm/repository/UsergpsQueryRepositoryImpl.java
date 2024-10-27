baed3588948aa145d82b958bb09b4f64e22cf277
ManchanTime <lych0918@gachon.ac.kr>
2024-10-27 14:03:31 +0900

    fix Query문 여러 지역 적용되게 수정

--- src/main/java/transtreaming/FCM/domain/fcm/repository/UsergpsQueryRepositoryImpl.java
+++ src/main/java/transtreaming/FCM/domain/fcm/repository/UsergpsQueryRepositoryImpl.java
@@ -1,27 +1,49 @@
 package transtreaming.FCM.domain.fcm.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import transtreaming.FCM.domain.fcm.dto.info.TokenInfo;
 
 import java.util.List;
 
 import static transtreaming.FCM.domain.fcm.entity.QUsergps.usergps;
 
-
+@Slf4j
 @RequiredArgsConstructor
 public class UsergpsQueryRepositoryImpl implements UsergpsQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
     public List<TokenInfo> findByRegion(String region) {
+        String[] addresses = region.split(",");
+        String pronounce = addresses[0].split(" ")[0];
+        BooleanBuilder builder = builder(addresses, pronounce);
+        for (String address : addresses) {
+            address = address.trim();
+            if(address.startsWith(region)) {
+                builder.or(usergps.address.like(address + "%"));
+            }
+        }
         return queryFactory.select(
                 Projections.constructor(TokenInfo.class,
                         usergps.deviceToken
                 ))
                 .from(usergps)
-                .where(usergps.address.like(region + "%"))
+                .where(builder)
                 .fetch();
+    }
+
+    private BooleanBuilder builder(String[] addresses, String pronounce) {
+        BooleanBuilder builder = new BooleanBuilder();
+        for (String address : addresses) {
+            address = address.trim();
+            if(address.startsWith(pronounce)) {
+                builder.or(usergps.address.like(address + "%"));
+            }
+        }
+        return builder;
     }
 }
